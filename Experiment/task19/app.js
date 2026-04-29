c52b5452c9089d17b44b9ebe03f4a102dd524e6c
Himanshi <himanshisharma6321@gmail.com>
2026-04-29 10:23:31 +0530

    task done

--- Experiment/task19/app.js
+++ Experiment/task19/app.js
@@ -1,18 +1,14 @@
 const express = require('express');
 const xss = require('xss-clean');
-
 const app = express();
-
 app.use(express.json());
 app.use(xss());
-
 app.post('/student',(req,res)=>{
    res.send({
       message:"Input sanitized successfully",
       data:req.body
    });
 });
-
 app.listen(3000,()=>{
  console.log("Server running on port 3000");
 });
